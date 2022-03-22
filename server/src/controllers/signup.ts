d7e19be3b5142dee9bb710e0576e97b30a45ff0e
최형진 <jin@Jin-MacBookAir.local>
2022-03-22 23:37:05 +0900

    [dev] add routers,controllers

---
+++ server/src/controllers/signup.ts
@@ -0,0 +1,17 @@
+import { Request, Response } from "express";
+import { createQueryBuilder, getRepository, getConnection, MetadataAlreadyExistsError } from "typeorm";
+import { User } from '../entity/User';
+
+
+const SignUp = async (req: Request, res: Response) => {
+    const {email,nickname,password}=req.body
+    const userRepo = getRepository(User)
+    const user=new User()
+    user.email=email
+    user.nickname=nickname
+    user.password=password
+    await userRepo.save(user)
+    res.status(201).json({message:'ok'})
+}
+
+export default SignUp
