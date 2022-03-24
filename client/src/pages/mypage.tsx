dffb8d36c393ec472fc56b3457336b758a1507ab
최형진 <jin@Jin-MacBookAir.local>
2022-03-25 01:26:22 +0900

    [dev] fix css

--- client/src/pages/mypage.tsx
+++ client/src/pages/mypage.tsx
@@ -2,8 +2,23 @@ import { URL } from '../url'
 import { useEffect, useState } from 'react'
 import axios from 'axios'
 import Loader from '../components/loader'
+import styled from 'styled-components'
 
-
+const Headers = styled.div`
+    font-size: 2rem;
+`
+const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    background-color: whitesmoke;
+    border-radius: 10px;
+    border: 2px solid #F1F1F1;
+    width: 1100px;
+    justify-content: center;
+    align-items: center;
+    height: 500px;
+    margin: auto;
+`
 function Mypage() {
     const [userInfo, setUserInfo] = useState<any>('')
     const [loading, setLoading] = useState(false)
@@ -31,13 +46,13 @@ function Mypage() {
     if (loading) return <Loader type="spin" color="#999999" />
 
     return (
-        <>
-            <div>mypage</div>
-            <div>{userInfo.nickname}</div>
-            <div>{userInfo.id}</div>
-            <div>{userInfo.password}</div>
-            <div>{userInfo.email}</div>
-        </>
+        <Container>
+            <Headers>mypage</Headers>
+            <div>nickname : {userInfo.nickname}</div>
+            <div>userId : {userInfo.id}</div>
+            <div>Password : {userInfo.password}</div>
+            <div>email : {userInfo.email}</div>
+        </Container>
 
     )
 }
