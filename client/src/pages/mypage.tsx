442d4a05e0c19f98b2811c307e1b9dbff6d2af62
최형진 <jin@Jin-MacBookAir.local>
2022-03-30 22:05:35 +0900

    [dev] fix mypage

--- client/src/pages/mypage.tsx
+++ client/src/pages/mypage.tsx
@@ -1,60 +1,119 @@
-import { URL } from '../url'
-import { useEffect, useState } from 'react'
-import axios from 'axios'
-import Loader from '../components/loader'
-import styled from 'styled-components'
+// import { URL } from '../url'
+// import { useEffect, useState } from 'react'
+// import axios from 'axios'
+// import Loader from '../components/loader'
+// import styled from 'styled-components'
 
-const Headers = styled.div`
-    font-size: 2rem;
-`
-const Container = styled.div`
-    display: flex;
-    flex-direction: column;
-    background-color: whitesmoke;
-    border-radius: 10px;
-    border: 2px solid #F1F1F1;
-    width: 1100px;
-    justify-content: center;
-    align-items: center;
-    height: 500px;
-    margin: auto;
-`
-function Mypage() {
-    const [userInfo, setUserInfo] = useState<any>('')
-    const [loading, setLoading] = useState(false)
+// const Headers = styled.div`
+//     font-size: 2rem;
+// `
+// const Container = styled.div`
+//     display: flex;
+//     flex-direction: column;
+//     background-color: whitesmoke;
+//     border-radius: 10px;
+//     border: 2px solid #F1F1F1;
+//     width: 1100px;
+//     justify-content: center;
+//     align-items: center;
+//     height: 500px;
+//     margin: auto;
+// `
+// function Mypage() {
+//     const [userInfo, setUserInfo] = useState<any>('')
+//     const [loading, setLoading] = useState(false)
 
-    useEffect(() => {
-        fetchData()
-        return () => {
-            setLoading(false)
-        }
-    }, [])
+//     useEffect(() => {
+//         fetchData()
+//         return () => {
+//             setLoading(false)
+//         }
+//     }, [])
 
-    async function fetchData() {
-        try {
-            const user = await axios.get(`${URL}/getuser`, config)
-            setUserInfo(user.data.data)
-        } catch (err) {
-            console.log(err)
-        }
+//     async function fetchData() {
+//         try {
+//             const user = await axios.get(`${URL}/getuser`, config)
+//             setUserInfo(user.data.data)
+//         } catch (err) {
+//             console.log(err)
+//         }
+//     }
+//     const config = {
+//         headers: { "Content-type": "application/json" },
+//         withCredentials: true
+//     }
+
+//     if (loading) return <Loader type="spin" color="#999999" />
+
+//     return (
+//         <Container>
+//             <Headers>mypage</Headers>
+//             <div>nickname : {userInfo.nickname}</div>
+//             <div>userId : {userInfo.id}</div>
+//             <div>Password : {userInfo.password}</div>
+//             <div>email : {userInfo.email}</div>
+//         </Container>
+
+//     )
+// }
+
+// export default Mypage
+import React, { useState, useEffect } from 'react';
+import { Link/* , useNavigate */ } from 'react-router-dom';
+import styled from 'styled-components';
+import axios from 'axios';
+
+
+const MyPageWrap = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  width: 100%;
+  height: 100%;
+  padding-top: 100px;
+`;
+const InfoWrap = styled.div`
+width: 345px;
+  margin-right: 150px;
+`;
+
+const Info = styled.div`
+  .btn {
+    text-decoration: none;
+    &:visited {
+      color: black;
     }
-    const config = {
-        headers: { "Content-type": "application/json" },
-        withCredentials: true
+    &:hover {
+      color: gray;
     }
+  }
+  
+`;
 
-    if (loading) return <Loader type="spin" color="#999999" />
+function MyPage () {
+  const [loading, setLoading] = useState(false);
+  const [itemReviewed, setItemReviewed] = useState([]);
+  const config = {
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    withCredentials: true
+  };
 
-    return (
-        <Container>
-            <Headers>mypage</Headers>
-            <div>nickname : {userInfo.nickname}</div>
-            <div>userId : {userInfo.id}</div>
-            <div>Password : {userInfo.password}</div>
-            <div>email : {userInfo.email}</div>
-        </Container>
 
-    )
+
+  // console.log(itemReviewed);
+
+  return (
+    <MyPageWrap>
+      <InfoWrap>
+        <h1> 님 환영합니다 !</h1>
+        <Info>
+          <div></div>
+        </Info>
+      </InfoWrap>
+    </MyPageWrap>
+  );
 }
 
-export default Mypage
+export default MyPage;
