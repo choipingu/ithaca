c59aa5639376e84a21035ab92c3f3b5ed1d70b6e
최형진 <jin@Jin-MacBookAir.local>
2022-04-09 23:54:37 +0900

    [dev] add delete, status

--- server/src/boards/boards.controller.ts
+++ server/src/boards/boards.controller.ts
@@ -1,5 +1,5 @@
-import { Body, Controller, Get, Post} from '@nestjs/common';
-import { Board } from './board.model';
+import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import { Board, BoardStatus } from './board.model';
 import { BoardsService } from './boards.service';
 import { CreateBoardDto } from './dto/create-board.dto';
 
@@ -18,8 +18,27 @@ export class BoardsController {
     // }
     @Post()
     createBoard(
-        @Body() createBoardDto:CreateBoardDto
-        ): Board {
-            return this.boardsService.createBoard(createBoardDto)
-        }
+        @Body() createBoardDto: CreateBoardDto
+    ): Board {
+        return this.boardsService.createBoard(createBoardDto)
+    }
+
+    @Get('/:id')
+    getBoardById(@Param('id') id: string): Board {
+        return this.boardsService.getBoardById(id)
+    }
+
+    @Delete('/:id')
+    deleteBoard(@Param('id') id: string): void {
+        this.boardsService.deleteBoard(id)
+    }
+
+    @Patch('/:id/status')
+    updateBoardStatus(
+        @Param('id') id: string,
+        @Body('status') status: BoardStatus
+    ) {
+        return this.boardsService.updateBoardStatus(id,status)
+    }
+
 }
