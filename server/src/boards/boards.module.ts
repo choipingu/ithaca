8a5dfce96de2a95b3d5fa96f0f278ef64134b9df
최형진 <jin@Jin-MacBookAir.local>
2022-04-10 23:58:29 +0900

    [dev] add pipe

--- server/src/boards/boards.module.ts
+++ server/src/boards/boards.module.ts
@@ -2,6 +2,10 @@ import { Module } from '@nestjs/common';
 import { BoardsController } from './boards.controller';
 import { BoardsService } from './boards.service';
 
+// nest g module boards --no-spec
+//nest : using nestcli
+//g : generate (사용한다)
+//--no-spec : 테스트 코드 생성 안함
 @Module({
   controllers: [BoardsController],
   providers: [BoardsService]
--- server/src/boards/boards.service.ts
+++ server/src/boards/boards.service.ts
@@ -1,8 +1,10 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Board, BoardStatus } from './board.model';
 import { v1 as uuid} from 'uuid' // 데이터베이스에서는 자동으로 유니크값을 넣어주지만 로컬에선 넣어줄수 없으므로 uuid라는 패키지를 받아서 사용
 import { CreateBoardDto } from './dto/create-board.dto';
 
+// nset g service boards --no-spec
+// Injectable 를 사용하게 되면 에플리케이션 전체에서 사용될수 있음 예를들어 컨트롤 A에서 사용되던 서비스를 컨트롤 B에도 같은 서비스를 사용할수 있게 한다
 @Injectable()
 export class BoardsService {
     private boards:Board[] = [] //private를 사용한 이유는 다른 컴포넌트에서 수정을 할수 있기 때문에 차단하기 위해 사용
@@ -23,7 +25,12 @@ export class BoardsService {
         return board
     }
     getBoardById(id:string): Board{
-        return this.boards.find((board)=> board.id ===id)
+        const found = this.boards.find((board)=> board.id ===id)
+
+        if(!found){
+            throw new NotFoundException(`Can't find Board with id ${id}`)
+        }
+        return found
     }
     deleteBoard(id:string):void{
         this.boards=this.boards.filter((board)=> board.id !== id)
