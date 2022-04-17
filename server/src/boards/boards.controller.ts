e78b901307a922064e2b5e44ec52dceef9a803e5
최형진 <jin@Jin-MacBookAir.local>
2022-04-17 23:37:53 +0900

    [dev] connect database

--- server/src/boards/boards.controller.ts
+++ server/src/boards/boards.controller.ts
@@ -1,5 +1,6 @@
 import { Body, Controller, Delete, Get, Param, Patch, Post, UsePipes, ValidationPipe } from '@nestjs/common';
-import { Board, BoardStatus } from './board.model';
+import { BoardStatus } from './board-status.enum';
+import { Board } from './board.entitiy';
 import { BoardsService } from './boards.service';
 import { CreateBoardDto } from './dto/create-board.dto';
 import { BoardStatusValidationPipe } from './pipes/board-status-validation.pipe';
@@ -9,42 +10,42 @@ import { BoardStatusValidationPipe } from './pipes/board-status-validation.pipe'
 export class BoardsController {
     constructor(private boardsService: BoardsService) { }
 
-    @Get() // 모든 게시물 가져오기
-    getAllBoard(): Board[] {
-        return this.boardsService.getAllBoards()
-    }
+    // @Get() // 모든 게시물 가져오기
+    // getAllBoard(): Board[] {
+    //     return this.boardsService.getAllBoards()
+    // }
 
-    /* 
-    @Post()
-    createBoard(@Body() body){
-        console.log('body',body)
-    } 
-    */
+    // /* 
+    // @Post()
+    // createBoard(@Body() body){
+    //     console.log('body',body)
+    // } 
+    // */
 
-    @Post() //게시물 생성
-    @UsePipes(ValidationPipe) //핸들러 파이프
-    createBoard(
-        @Body() createBoardDto: CreateBoardDto
-    ): Board {
-        return this.boardsService.createBoard(createBoardDto)
-    }
+    // @Post() //게시물 생성
+    // @UsePipes(ValidationPipe) //핸들러 파이프
+    // createBoard(
+    //     @Body() createBoardDto: CreateBoardDto
+    // ): Board {
+    //     return this.boardsService.createBoard(createBoardDto)
+    // }
 
     @Get('/:id') //특정 게시물 가져오기
-    getBoardById(@Param('id') id: string): Board {
+    getBoardById(@Param('id') id: number): Promise<Board> {
         return this.boardsService.getBoardById(id)
     }
 
-    @Delete('/:id') //특정 게시물 삭제하기
-    deleteBoard(@Param('id') id: string): void {
-        this.boardsService.deleteBoard(id)
-    }
+    // @Delete('/:id') //특정 게시물 삭제하기
+    // deleteBoard(@Param('id') id: string): void {
+    //     this.boardsService.deleteBoard(id)
+    // }
 
-    @Patch('/:id/status') // 게시물 상태 업데이트하기
-    updateBoardStatus(
-        @Param('id') id: string,
-        @Body('status', BoardStatusValidationPipe) status: BoardStatus
-    ) {
-        return this.boardsService.updateBoardStatus(id,status)
-    }
+    // @Patch('/:id/status') // 게시물 상태 업데이트하기
+    // updateBoardStatus(
+    //     @Param('id') id: string,
+    //     @Body('status', BoardStatusValidationPipe) status: BoardStatus
+    // ) {
+    //     return this.boardsService.updateBoardStatus(id,status)
+    // }
 
 }
--- server/src/boards/boards.service.ts
+++ server/src/boards/boards.service.ts
@@ -1,45 +1,51 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
-import { Board, BoardStatus } from './board.model';
+import { BoardStatus } from './board-status.enum';
 import { v1 as uuid} from 'uuid' // 데이터베이스에서는 자동으로 유니크값을 넣어주지만 로컬에선 넣어줄수 없으므로 uuid라는 패키지를 받아서 사용
 import { CreateBoardDto } from './dto/create-board.dto';
+import { BoardRepository } from './board.repository';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Board } from './board.entitiy';
 
 // nset g service boards --no-spec
 // Injectable 를 사용하게 되면 에플리케이션 전체에서 사용될수 있음 예를들어 컨트롤 A에서 사용되던 서비스를 컨트롤 B에도 같은 서비스를 사용할수 있게 한다
 @Injectable()
 export class BoardsService {
-    private boards:Board[] = [] //private를 사용한 이유는 다른 컴포넌트에서 수정을 할수 있기 때문에 차단하기 위해 사용
+    constructor(
+        @InjectRepository(BoardRepository)
+        private boardRepository: BoardRepository
+        ){}
 
-    getAllBoards(): Board[]{ // 모든 게시물 가져오기
-        return this.boards
-    }
+    // getAllBoards(): Board[]{ // 모든 게시물 가져오기
+    //     return this.boards
+    // }
 
-    createBoard(createBoardDto : CreateBoardDto){ // 게시물 생성
-        const {title, description} = createBoardDto
-        const board :Board= {
-            id: uuid(),
-            title,
-            description,
-            status: BoardStatus.PUBLIC
-        }
-        this.boards.push(board)
-        return board
-    }
-    getBoardById(id:string): Board{ //특정 게시물 찾기
-        const found = this.boards.find((board)=> board.id ===id)
+    // createBoard(createBoardDto : CreateBoardDto){ // 게시물 생성
+    //     const {title, description} = createBoardDto
+    //     const board :Board= {
+    //         id: uuid(),
+    //         title,
+    //         description,
+    //         status: BoardStatus.PUBLIC
+    //     }
+    //     this.boards.push(board)
+    //     return board
+    // }
+    async getBoardById(id:number): Promise <Board>{ //특정 게시물 찾기
+        const found = await this.boardRepository.findOne(id)
 
         if(!found){
             throw new NotFoundException(`Can't find Board with id ${id}`)
         }
         return found
     }
-    deleteBoard(id:string):void{ // 특정 게시물 삭제
-        const found = this.getBoardById(id)
-        this.boards=this.boards.filter((board)=> board.id !== found.id)
-    }
-    updateBoardStatus(id: string , status: BoardStatus): Board{ // 게시물 상태 업데이트
-        const board = this.getBoardById(id)
-        board.status=status
-        return board
+    // deleteBoard(id:string):void{ // 특정 게시물 삭제
+    //     const found = this.getBoardById(id)
+    //     this.boards=this.boards.filter((board)=> board.id !== found.id)
+    // }
+    // updateBoardStatus(id: string , status: BoardStatus): Board{ // 게시물 상태 업데이트
+    //     const board = this.getBoardById(id)
+    //     board.status=status
+    //     return board
 
-    }
+    // }
 }
