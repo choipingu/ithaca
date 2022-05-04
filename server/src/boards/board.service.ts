543fafe29ebdda714de73d730a86e91b6d64e31e
최형진 <jin@Jin-MacBookAir.local>
2022-05-04 23:13:52 +0900

    [dev] add fk

--- server/src/boards/board.service.ts
+++ server/src/boards/board.service.ts
@@ -5,6 +5,7 @@ import { CreateBoardDto } from './dto/create-board.dto';
 import { BoardRepository } from './board.repository';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Board } from './board.entity';
+import { User } from 'src/users/user.entity';
 
 // nset g service boards --no-spec
 // Injectable 를 사용하게 되면 에플리케이션 전체에서 사용될수 있음 예를들어 컨트롤 A에서 사용되던 서비스를 컨트롤 B에도 같은 서비스를 사용할수 있게 한다
@@ -19,8 +20,8 @@ export class BoardService {
         return await this.boardRepository.find()
     }
 
-    createBoard(createBoardDto : CreateBoardDto): Promise<Board>{ // 게시물 생성
-        return this.boardRepository.createBoard(createBoardDto)
+    createBoard(createBoardDto : CreateBoardDto, user:User): Promise<Board>{ // 게시물 생성
+        return this.boardRepository.createBoard(createBoardDto,user)
     }
     getBoardById(id:number): Promise <Board>{ //특정 게시물 찾기
         return this.boardRepository.getBoardById(id)       
