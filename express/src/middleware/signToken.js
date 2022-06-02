9202a82e2c2cff00d631ad252a3435c6bde60660
최형진 <jin@Jin-MacBookAir.local>
2022-06-02 22:51:10 +0900

    [dev] add edit

--- express/src/middleware/signToken.js
+++ express/src/middleware/signToken.js
@@ -4,6 +4,5 @@ import * as dotenv from 'dotenv'
 dotenv.config()
 
 export async function signToken(userInfo) {
-    delete userInfo.password
     return jwt.sign({ userInfo }, process.env.ACCESS_SECRET, { expiresIn: '10m' })
 }
